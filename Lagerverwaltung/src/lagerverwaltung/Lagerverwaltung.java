733d0ce20e198757c9cd0cc9d090aa66d58a58bc
Frederic Bahr <fredericbahr1@gmail.com>
2019-08-05 19:26:33 +0200

    Test

--- Lagerverwaltung/src/lagerverwaltung/Lagerverwaltung.java
+++ Lagerverwaltung/src/lagerverwaltung/Lagerverwaltung.java
@@ -19,6 +19,7 @@ public class Lagerverwaltung {
 		gui = new LagerverwaltungGUI();
 		daten = new LagerverwaltungDaten();
 		gui.setDaten(daten);
+		//Test
 	}
 	
 	public static void main(String[] args) {
