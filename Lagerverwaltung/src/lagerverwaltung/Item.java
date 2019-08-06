e81bc85eefa02d3d7a52e0f5ecfa7c2f2e45f3ea
johan <johan@192.168.0.11>
2019-08-06 14:02:23 +0200

    Klasse erstellt

---
+++ Lagerverwaltung/src/lagerverwaltung/Item.java
@@ -0,0 +1,13 @@
+package lagerverwaltung;
+
+public class Item {
+	private String name;
+	private int teilenummer;
+	private int größe;
+	
+	public Item(String name, int teilenummer, int größe) {
+		this.name = name;
+		this.teilenummer = teilenummer;
+		this.größe = größe;
+	}
+}
